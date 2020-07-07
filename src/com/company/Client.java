e76ea97b457e1e2124c4a098178be268c559d25e
Takcaa <dawidkawka11@gmail.com>
2020-07-07 10:29:12 +0200

    Client + subcontractor with generators

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -3,9 +3,72 @@ package com.company;
 public class Client {
     public String name;
     private int type;
+    private double invoiceDelayChance;
+    private double delayPenaltyChance;
+    private double breakContactChance;
+    private double notPaid;
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public double getInvoiceDelayChance() {
+        return invoiceDelayChance;
+    }
+
+    public void setInvoiceDelayChance(double invoiceDelayChance) {
+        this.invoiceDelayChance = invoiceDelayChance;
+    }
+
+    public double getDelayPenaltyChance() {
+        return delayPenaltyChance;
+    }
+
+    public void setDelayPenaltyChance(double delayPenaltyChance) {
+        this.delayPenaltyChance = delayPenaltyChance;
+    }
+
+    public double getBreakContactChance() {
+        return breakContactChance;
+    }
+
+    public void setBreakContactChance(double breakContactChance) {
+        this.breakContactChance = breakContactChance;
+    }
+
+    public double getNotPaid() {
+        return notPaid;
+    }
+
+    public void setNotPaid(double notPaid) {
+        this.notPaid = notPaid;
+    }
 
     public Client(String name, int type) {
         this.name = name;
         this.type = type;
+        if(this.type == 1){
+            this.invoiceDelayChance = 30;
+            this.delayPenaltyChance = 20;
+            this.breakContactChance = 0;
+            this.notPaid = 0;
+        }
+        if(this.type == 2){
+            this.invoiceDelayChance = 0;
+            this.delayPenaltyChance = 0;
+            this.breakContactChance = 50;
+            this.notPaid = 0;
+        }
+        if(this.type == 3){
+            this.invoiceDelayChance = 30;
+            this.delayPenaltyChance = 20;
+            this.breakContactChance = 100;
+            this.notPaid = 1;
+        }
+
     }
 }
--- src/com/company/subcontractor.java
+++ src/com/company/subcontractor.java
@@ -1,6 +1,29 @@
 package com.company;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class subcontractor {
-    public String imie;
-    public int type;
+    private int type;
+    private double mistakeChance;
+    private double cost;
+    private double possibleDelay; //days
+
+    public subcontractor(int type, double mistakeChance, double cost) {
+        this.type = type;
+        this.cost = 10 * (type + 1) * 10;
+        this.possibleDelay = 0;
+        if (this.type == 1){
+            this.mistakeChance = 0;
+        }
+        if (this.type == 2){
+            this.mistakeChance = 20;
+        }
+        if (this.type == 3){
+            this.mistakeChance = 0;
+            int i =  ThreadLocalRandom.current().nextInt(1, 5 + 1);
+            if (i == 3){
+                this.possibleDelay = 10;
+            }
+        }
+    }
 }
